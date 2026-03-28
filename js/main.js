b58e4ae496937e07786d774bfa964a024499704a
Hodaya Eisencout <hodaya.eisen@gmail.com>
2026-03-29 00:27:19 +0300

    Increase ball size on click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 function onBallClick() {
-  console.log('Ball clicked');
+    var elBall = document.querySelector('.ball')
+
+  var currentSize = elBall.offsetWidth
+  var newSize = currentSize + 50
+
+  elBall.style.width = newSize + 'px'
+  elBall.style.height = newSize + 'px'
+
+  elBall.innerText = newSize
 }
